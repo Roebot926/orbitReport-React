e10f45d68763258d97866ca1892aa004547b8aa1
Jordan <138933858+Roebot926@users.noreply.github.com>
2023-08-15 11:45:22 -0400

    Assignment 6 work

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,31 @@
+import "./styling.css";
+import "../App"
+import React from "react";
+
 const Table = ({ sat }) => {
+  
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Sat Type</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data,id) => {
+            return(
+              <tr key={id}>
+                <td>{data.name}</td>
+                <td>{data.type}</td>
+                <td>{data.launchDate}</td>
+                {data.operational ? <td>Active</td> : <td>Inactive</td>}
+              </tr>
+            )
+          })};
+
         </tbody>
       </table>
   );
